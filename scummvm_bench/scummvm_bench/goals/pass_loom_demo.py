668d253763c8b9cf80763823e6746d8d22feb796
Xavier Hardy <xavierhardy@users.noreply.github.com>
2026-06-14 13:38:42 +0200

    MCP_BENCH: adding even more games

---
+++ scummvm_bench/scummvm_bench/goals/pass_loom_demo.py
@@ -0,0 +1,26 @@
+# Goals
+# state in first room
+# get to another room (walk left several times)
+# state in second room
+# walk to tents (room transition)
+# state in third room
+# go to tent on the left
+# state in tent
+# walk to elders on the right
+# pick up distaff
+# interact with egg: (message: "It's trying to open!")
+# play eced on egg ( messa;ge: "Thanks goodness you're still here!")
+# go to forest (room: 40)
+# "There's an owl in there!"
+# "This has an owl too!"
+# "Another owl!"
+# "Looks as if all holes are full"
+# Go to other tent (room: 38)
+# state in tent
+# play cccc on darkness (changes to room: 42)
+# interact with book
+# interact with dye pot
+# dye something in green
+# play eced on sky (open the sky) in the first room
+# go to the dock
+# use tree to leave the island (room change 36), end of demo
